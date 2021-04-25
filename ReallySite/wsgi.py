02b4f1b2c59247e8079fd59f4475c3e3c2b2fde0
ShigiMizuki <shifi1204@icloud.com>
2021-04-25 15:07:32 +0900

    opt apps

--- ReallySite/wsgi.py
+++ ReallySite/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 import sys
-sys.path.append('/home/bitnami/htdocs/tbs/ReallySite/ReallySite')
-# os.environ.setdefault("PYTHON_EGG_CACHE", "/home/bitnami/htdocs/tbs/ReallySite/egg_cache")
+sys.path.append('/opt/bitnami/apps/django/django_projects/ReallySite/ReallySite')
+# os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/ReallySite/egg_cache")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ReallySite.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
