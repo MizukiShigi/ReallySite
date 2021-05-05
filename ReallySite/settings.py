c6939e9133fd7cfed1480fbdce5e4f17161c72ad
ShigiMizuki <shifi1204@icloud.com>
2021-05-05 23:42:13 +0900

    alter conf

--- ReallySite/settings.py
+++ ReallySite/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = '/var/www/collected_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
